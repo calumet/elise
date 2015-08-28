3ae2ed54eb737144a7a714b14e9adcb0e745ffb6
Romel Pérez <ronelprhone@gmail.com>
2015-08-28 08:00:34 -0500

    feat: elise v0.8-beta

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,74 +4,74 @@
 
 module.exports = exports = function (grunt) {
 
-    grunt.loadNpmTasks('grunt-contrib-concat');
-    grunt.loadNpmTasks('grunt-contrib-uglify');
-    grunt.loadNpmTasks('grunt-contrib-cssmin');
-    grunt.loadNpmTasks('grunt-contrib-copy');
+  grunt.loadNpmTasks('grunt-contrib-concat');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-copy');
 
-    grunt.initConfig({
+  grunt.initConfig({
 
-        pkg: grunt.file.readJSON('package.json'),
+    pkg: grunt.file.readJSON('package.json'),
 
-        concat: {
-            options: {
-                separator: '\n\n',
-                banner: '/*! <%=pkg.name%>'
-                       +'\n * @version <%=pkg.version%>'
-                       +'\n * @release <%= grunt.template.today("yyyy-mm-dd") %>'
-                       +'\n * @url <%=pkg.repository.url%>'
-                       +'\n */\n\n'
-            },
-            javascript: {
-                src: [
-                    'js/elise.core.js',
-                    'js/elise.tip.js',
-                    'js/elise.alert.js',
-                    'js/elise.modal.js',
-                    'js/elise.loader.js',
-                    'js/elise.hope.js'
-                ],
-                dest: 'dist/js/elise.js',
-            },
-            css: {
-                src: [
-                    'css/elise.core.css',
-                    'css/elise.tip.css',
-                    'css/elise.alert.css',
-                    'css/elise.modal.css',
-                    'css/elise.loader.css',
-                    'css/elise.hope.css'
-                ],
-                dest: 'dist/css/elise.css',
-            }
-        },
+    concat: {
+      options: {
+        separator: '\n\n',
+        banner: '/*! <%=pkg.name%>'
+             +'\n * @version <%=pkg.version%>'
+             +'\n * @release <%= grunt.template.today("yyyy-mm-dd") %>'
+             +'\n * @url <%=pkg.repository.url%>'
+             +'\n */\n\n'
+      },
+      javascript: {
+        src: [
+          'js/core.js',
+          'js/tip.js',
+          'js/alert.js',
+          'js/modal.js',
+          'js/loader.js',
+          'js/hope.js'
+        ],
+        dest: 'dist/js/elise.js',
+      },
+      css: {
+        src: [
+          'css/core.css',
+          'css/tip.css',
+          'css/alert.css',
+          'css/modal.css',
+          'css/loader.css',
+          'css/hope.css'
+        ],
+        dest: 'dist/css/elise.css',
+      }
+    },
 
-        uglify: {
-            options: {
-                preserveComments: 'some'
-            },
-            javascript: {
-                src: 'dist/js/elise.js',
-                dest: 'dist/js/elise.min.js'
-            }
-        },
+    uglify: {
+      options: {
+        preserveComments: 'some'
+      },
+      javascript: {
+        src: 'dist/js/elise.js',
+        dest: 'dist/js/elise.min.js'
+      }
+    },
 
-        cssmin: {
-            css: {
-                src: 'dist/css/elise.css',
-                dest: 'dist/css/elise.min.css'
-            }
-        },
+    cssmin: {
+      css: {
+        src: 'dist/css/elise.css',
+        dest: 'dist/css/elise.min.css'
+      }
+    },
 
-        copy: {
-            images: {
-                expand: true,
-                cwd: 'img/elise/',
-                src: '**/*',
-                dest: 'dist/img/elise/'
-            }
-        }
-    });
+    copy: {
+      images: {
+        expand: true,
+        cwd: 'img/elise/',
+        src: '**/*',
+        dest: 'dist/img/elise/'
+      }
+    }
+  });
 
-    grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy']);
+  grunt.registerTask('default', ['concat', 'uglify', 'cssmin', 'copy']);
 };
